77b86d6c195e79b22f9a304a986b138d1de25e01
ujjwal-07 <23106049@Apsit.edu.in>
2026-02-15 11:20:43 +0530

    changes

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,12 +2,6 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI!;
 
-if (!MONGODB_URI) {
-    throw new Error(
-        'Please define the MONGODB_URI environment variable inside .env'
-    );
-}
-
 interface MongooseCache {
     conn: typeof mongoose | null;
     promise: Promise<typeof mongoose> | null;
@@ -32,6 +26,13 @@ export async function connectDB() {
         const opts = {
             bufferCommands: false,
         };
+
+        const MONGODB_URI = process.env.MONGODB_URI;
+        if (!MONGODB_URI) {
+            throw new Error(
+                'Please define the MONGODB_URI environment variable inside .env'
+            );
+        }
 
         cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
             return mongoose;

ee73749b8ed952c696db298356c1d35e0d06c4ec
ujjwal-07 <23106049@Apsit.edu.in>
2026-02-15 11:15:18 +0530

    changes in auth.config

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -54,4 +54,15 @@ export const authConfig = {
     session: {
         strategy: "jwt",
     },
+    cookies: {
+        sessionToken: {
+            name: `__Secure-next-auth.session-token`,
+            options: {
+                httpOnly: true,
+                sameSite: 'lax',
+                path: '/',
+                secure: true,
+            },
+        },
+    },
 } satisfies NextAuthConfig
